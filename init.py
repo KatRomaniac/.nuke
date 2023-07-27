605c375875c7bbd529fe13ddc01917acd148456a
KatRomaniac <140761487+KatRomaniac@users.noreply.github.com>
2023-07-27 16:34:21 -0600

    init.py

--- init.py
+++ init.py
@@ -1,1 +1,9 @@
+#-----------------------------
+#init.py
+#Version: 1.0.0
+#Last_Updated:_July_27th_2023
+#------DEFINE COUSTOM FOLDER STRUCTURE-----#
 
+
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./python')
