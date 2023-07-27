e8ac459fad8957328477d7e39a782e738006234e
KatRomaniac <140761487+KatRomaniac@users.noreply.github.com>
2023-07-27 16:34:44 -0600

    menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,26 @@
+#---------------------------------
+# menu.py
+# Version: 1.0.0
+# Last Updated: July 27th, 2023
+#---------------------------------
 
+import nuke
+import platform
+
+#.Deine where .nuke directory is on each OS's network
+
+Win_Dir = 'C:\Users\Kasha/.nuke'
+
+MacOSX_Dire = ''
+Linux_Dir = '/home/Kasha/.nuke'
+
+# Set global directory
+
+if platform.system() == "Windows":
+	dir = Win_Dir
+elif platform.system() == "Darwin":
+	dir = MacOSX_Dir
+elif platform.system() == "Linux":
+	dir = Linux_Dir
+else:
+	dir = None
